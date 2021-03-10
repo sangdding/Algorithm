7a2f770500bd9d7bc922d93ffc302710e0fdc954
sangdding <enjxz97382@naver.com>
2021-03-10 18:49:35 +0900

    first commit

---
+++ 1260.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+    int a = 0;
+    int i = 0;
+    int arr[11] = {};
+    for(i = 0; i < 9; i++) {
+        cin >> arr[i];
+    }
+    cout << arr[4] << endl;
+}
